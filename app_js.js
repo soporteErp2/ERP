72098617c5d5f711adc6e71b12cd60a9dc58b5a4
jonatan2874 <jonatan2874@gmail.com>
2024-06-26 06:28:27 -0500

    maquetacion nuevo escritorio

---
+++ app_js.js
@@ -0,0 +1,9 @@
+function toogle_nav(){
+    let nav = document.getElementById("nav")
+    let content = document.getElementById("content")
+    
+    nav.classList.toggle("w-0")
+    // nav.classList.toggle("-translate-x-full")
+    // nav.classList.remove("w-60")
+    // nav.classList.add("w-0")
+}
