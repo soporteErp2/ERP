ab61fe3eece398da84b06e9f9b3c990248d0c73d
jonatan2874 <jonatan2874@gmail.com>
2024-06-28 01:47:36 -0500

    new desktop

--- app_js.js
+++
@@ -1,9 +0,0 @@
-function toogle_nav(){
-    let nav = document.getElementById("nav")
-    let content = document.getElementById("content")
-    
-    nav.classList.toggle("w-0")
-    // nav.classList.toggle("-translate-x-full")
-    // nav.classList.remove("w-60")
-    // nav.classList.add("w-0")
-}
---
+++ assets/js/app_js.js
@@ -0,0 +1,22 @@
+// functions
+function toggle_nav(){
+    let nav = document.getElementById("nav")
+    nav.classList.toggle("w-0")
+}
+
+function toggle_perfil(){
+    let profile = document.getElementById("profile-content")
+    profile.classList.toggle("opacity-0")
+    profile.classList.toggle("z-50")
+}
+
+//listeners
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('nav-btn').addEventListener('click', function() {
+        toggle_nav();
+    });
+    document.getElementById('profile-btn').addEventListener('click', function() {
+        toggle_perfil();
+    });
+});
+
