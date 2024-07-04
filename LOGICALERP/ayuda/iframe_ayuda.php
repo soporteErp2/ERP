6b569cf49bb4c7f9a1fe80504a6145d76da7ca64
Yerson Rozo <soporte.erp2@plataforma.com.co>
2024-07-04 11:38:50 -0500

    Sync: carpeta ayuda

---
+++ LOGICALERP/ayuda/iframe_ayuda.php
@@ -0,0 +1,7 @@
+<?php 
+
+	include("../../configuracion/conectar.php");
+	include("../../configuracion/define_variables.php");
+	echo '<iframe id="iframeEncuestasOrdenes" src="'.$url.'#zoom=150" width="'.$width.'" height="'.$height.'"></iframe>';
+
+?>
