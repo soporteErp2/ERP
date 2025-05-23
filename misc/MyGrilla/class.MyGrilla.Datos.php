2891c383fad1c41ae344e6b3f03c60644705953b
soporteERP <soporte.erp2@plataforma.com.co>
2025-05-23 06:57:24 -0500

    feat: el codigo ahora esta mas organizado y se agrega la funcinalidad de validaciones personalizadas trim y blanspaces a los campos

--- misc/MyGrilla/class.MyGrilla.Datos.php
+++ misc/MyGrilla/class.MyGrilla.Datos.php
@@ -1,112 +1,142 @@
 <?php
-		/////////////////////////////////////////////////////////////
-		if($this->LaOpcion == 'GuardaBD'){
+/////////////////////////////////////////////////////////////
+if ($this->LaOpcion == 'GuardaBD') {
 
-			$cadena = $this->VarPostSql;
-			$cadena = substr($cadena, 0, strrpos($cadena,'{.}'));
-			$MyVariables = explode('{.}',$cadena);
-			$contenido = array();
+    $cadena = $this->VarPostSql;
+    $cadena = substr($cadena, 0, strrpos($cadena, '{.}'));
+    $MyVariables = explode('{.}', $cadena);
+    $contenido = array();
 
+    $SQLCampos = "";
+    $SQLValores = "";
 
-			$SQL = "INSERT INTO ".$this->TableName2."(";
+    for ($i = 0; $i < count($MyVariables); $i++) {
+        $contenido[$i] = explode('{:}', $MyVariables[$i]);
+        $campo = $contenido[$i][0];
+        $valor = $contenido[$i][1];
 
-				for($i=0;$i<count($MyVariables);$i++){
-					$contenido[$i] = explode('{:}',$MyVariables[$i]);
-					$SQL .= $contenido[$i][0];
-					if($i<(count($MyVariables)-1)){$SQL .= ',';}
-				}
+        // Validaciones personalizadas por campo
+        for ($v = 0; $v < $this->CuantosValidations; $v++) {
+            if ($this->ValidacionesCampos[$v] == $campo) {
+                $tipo = $this->Validaciones[$v];
+                if ($tipo == 'trim') {
+                    $valor = trim($valor);
+                } elseif ($tipo == 'blankspaces') {
+                    $valor = str_replace(' ', '', $valor);
+                }
+            }
+        }
 
-				if(mysql_num_rows(mysql_query("SHOW COLUMNS FROM ".$this->TableName2." LIKE 'UserIdLog' ")) == 1 ){$SQL .= ",UserIdLog";}
+        $SQLCampos .= $campo;
+        $SQLValores .= "'" . $valor . "'";
 
-			$SQL .= ")VALUES(";
+        if ($i < (count($MyVariables) - 1)) {
+            $SQLCampos .= ',';
+            $SQLValores .= ',';
+        }
+    }
 
-				for($i=0;$i<count($MyVariables);$i++){
-					$contenido[$i] = explode('{:}',$MyVariables[$i]);
-					$SQL .= "'".$contenido[$i][1]."'";
-					if($i<(count($MyVariables)-1)){$SQL .= ',';}
-				}
+    if (mysql_num_rows(mysql_query("SHOW COLUMNS FROM ".$this->TableName2." LIKE 'UserIdLog' ")) == 1) {
+        $SQLCampos .= ",UserIdLog";
+        $SQLValores .= ",'" . $_SESSION["IDUSUARIO"] . "'";
+    }
 
-				if(mysql_num_rows(mysql_query("SHOW COLUMNS FROM ".$this->TableName2." LIKE 'UserIdLog' ")) == 1 ){$SQL .= ",'".$_SESSION["IDUSUARIO"]."'";}
+    $SQL = "INSERT INTO ".$this->TableName2."(".$SQLCampos.") VALUES(".$SQLValores.")";
 
-			$SQL .= ")";
+    $connectid = mysql_query($SQL, $this->Link);
 
-			//echo $SQL;
+    if ($connectid) {
+        $id = mysql_insert_id($this->Link);
+        echo 'true{.}'.$id.'{.}';
+        mylog('GRILLA('.$this->GrillaName.') -> '.$SQL, 4, $this->Link);
+    } else {
+        echo 'false{.}';
+        echo mysql_error().'{.}';
+        echo $SQL;
+    }
+}
 
-		   $connectid =mysql_query($SQL,$this->Link);
+/////////////////////////////////////////////////////////////
+if ($this->LaOpcion == 'ActualizaBD') {
 
-			if($connectid){
-				$id = mysql_insert_id($this->Link);
-				echo 'true{.}'.$id.'{.}';
-				mylog('GRILLA('.$this->GrillaName.') -> '.$SQL ,4,$this->Link);
-			}else{
-				echo 'false{.}';
-                echo mysql_error().'{.}';
-				echo $SQL;
+    $cadena = $this->VarPostSql;
+    $cadena = substr($cadena, 0, strrpos($cadena, '{.}'));
+    $MyVariables = explode('{.}', $cadena);
+    $contenido = array();
 
-			}
-		}
+    $SQL = "UPDATE ".$this->TableName2." SET ";
 
-		/////////////////////////////////////////////////////////////
-		if($this->LaOpcion == 'ActualizaBD'){
+    for ($i = 0; $i < count($MyVariables); $i++) {
+        $contenido[$i] = explode('{:}', $MyVariables[$i]);
+        $campo = $contenido[$i][0];
+        $valor = $contenido[$i][1];
 
-			$cadena      = $this->VarPostSql;
-			$cadena      = substr($cadena, 0, strrpos($cadena,'{.}'));
-			$MyVariables = explode('{.}',$cadena);
-			$contenido   = array();
+        // Validaciones personalizadas por campo
+        for ($v = 0; $v < $this->CuantosValidations; $v++) {
+            if ($this->ValidacionesCampos[$v] == $campo) {
+                $tipo = $this->Validaciones[$v];
+                if ($tipo == 'trim') {
+                    $valor = trim($valor);
+                } elseif ($tipo == 'blankspaces') {
+                    $valor = str_replace(' ', '', $valor);
+                }
+            }
+        }
 
-			$SQL = "UPDATE ".$this->TableName2." SET ";
+        // Sanitiza comillas
+        $valor = str_replace("'", "`", $valor);
+        $valor = str_replace('"', "``", $valor);
 
-				for($i=0;$i<count($MyVariables);$i++){
-					$contenido[$i] = explode('{:}',$MyVariables[$i]);
+        $SQL .= $campo . " = '" . $valor . "'";
 
-					$valor = $contenido[$i][1];
-					$valor = str_replace("'", "`", $valor);
-					$valor = str_replace('"', "``", $valor);
+        if ($i < (count($MyVariables) - 1)) {
+            $SQL .= ", ";
+        }
+    }
 
-					$SQL .= $contenido[$i][0]." = '".$valor."'";
+    if (mysql_num_rows(mysql_query("SHOW COLUMNS FROM ".$this->TableName2." LIKE 'UserIdLog' ")) == 1) {
+        $SQL .= ", UserIdLog = '".$_SESSION["IDUSUARIO"]."'";
+    }
 
-					if($i<(count($MyVariables)-1)){$SQL .= ',';}
-				}
-				if(mysql_num_rows(mysql_query("SHOW COLUMNS FROM ".$this->TableName2." LIKE 'UserIdLog' ")) == 1){$SQL .= ",UserIdLog = '".$_SESSION["IDUSUARIO"]."'";}
+    $SQL .= " WHERE id=".$this->VariableInUpDe;
+    $connectid = mysql_query($SQL, $this->Link);
 
+    if ($connectid) {
+        echo 'true{.}'.$this->VariableInUpDe.'{.}'.$this->LastUpdate;
+        mylog('GRILLA('.$this->GrillaName.') -> '.$SQL, 4, $this->Link);
+    } else {
+        echo 'false{.}';
+        echo mysql_error().'{.}';
+        echo $SQL;
+    }
+}
 
-			$SQL .=	" WHERE id=".$this->VariableInUpDe;
-		    $connectid = mysql_query($SQL,$this->Link);
+/////////////////////////////////////////////////////////////
+if ($this->LaOpcion == 'EliminaBD') {
 
-			if($connectid){
-				echo 'true{.}'.$this->VariableInUpDe.'{.}'.$this->LastUpdate;
-				mylog('GRILLA('.$this->GrillaName.') -> '.$SQL ,4,$this->Link);
-			}
-			else{
-				echo 'false{.}';
-                echo mysql_error().'{.}';
-				echo $SQL;
-			}
-		}
+    if ($this->VSqlBtnEliminar != 'false') {
+        $queryValidateBtn = mysql_query($this->VSqlBtnEliminar, $this->Link);
+        while ($row = mysql_fetch_array($queryValidateBtn)) {
+            echo '{.}trueSQL{.}';
+            return;
+        }
+    }
 
-		/////////////////////////////////////////////////////////////
-		if($this->LaOpcion == 'EliminaBD'){
+    if ($this->VComporEliminar == 'true') {
+        $SQL = "UPDATE ".$this->TableName2." SET activo = 0 WHERE id='".$this->VariableInUpDe."'";
+    } else {
+        $SQL = "DELETE FROM ".$this->TableName2." WHERE id='".$this->VariableInUpDe."'";
+    }
 
-			if($this->VSqlBtnEliminar != 'false'){
-				$queryValidateBtn = mysql_query($this->VSqlBtnEliminar,$this->Link);
-				while($row = mysql_fetch_array($queryValidateBtn)){ echo '{.}trueSQL{.}'; return; }
-			}
+    $connectid = mysql_query($SQL, $this->Link);
 
-			if($this->VComporEliminar == 'true'){
-				$SQL = "UPDATE ".$this->TableName2." SET activo = 0 WHERE id='".$this->VariableInUpDe."'";
-			}else{
-				$SQL = "DELETE FROM ".$this->TableName2." WHERE id='".$this->VariableInUpDe."'";
-			}
-
-			$connectid =mysql_query($SQL,$this->Link);
-
-			if($connectid){
-				echo  '{.}true{.}'.$this->VariableInUpDe.'{.}';
-				mylog('GRILLA('.$this->GrillaName.') -> '.$SQL ,4,$this->Link);
-			}else{
-				echo '{.}false{.}';
-				echo mysql_error().'{.}';
-				echo $SQL;
-			}
-		}
+    if ($connectid) {
+        echo '{.}true{.}'.$this->VariableInUpDe.'{.}';
+        mylog('GRILLA('.$this->GrillaName.') -> '.$SQL, 4, $this->Link);
+    } else {
+        echo '{.}false{.}';
+        echo mysql_error().'{.}';
+        echo $SQL;
+    }
+}
 ?>
